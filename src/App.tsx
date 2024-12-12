12587ecd9f755d31189ea5c6558ea3c43fd3dd47
biogolf10457 <biogolf10457@gmail.com>
2024-12-12 18:20:36 +0700

    fetch thecatapi and set the amount of pictures per fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  type Cat = {
+    id: string;
+    url: string;
+    width: number;
+    height: number;
+    breeds?: any;
+    favourite?: any;
+  };
+
+  const [images, setImages] = useState<Cat[]>([]);
+  const [amount, setAmount] = useState<number>(1);
+  const amountList = [1, 4, 9];
+  const name: string = "John Doe";
+
+  useEffect(() => {
+    refreshImage();
+  }, []);
+
+  async function refreshImage() {
+    try {
+      const response = await fetch(
+        `https://api.thecatapi.com/v1/images/search?limit=${amount}`,
+        {
+          headers: {
+            "x-api-key":
+              "live_fXPubrL0cMOfzexjHxWIAEoExyHhZrM98ywb8qeNah8fFqO972d64kfNztyvVgD1",
+          },
+        }
+      );
+      const json = await response.json();
+      setImages(json);
+      console.log(json);
+      console.log(amount);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>{name}</h1>
+      <div>
+        <label htmlFor="amount">Select amount per load</label>
+        <select
+          id="amount"
+          name="amount"
+          value={amount}
+          onChange={(e) => setAmount(+e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          {amountList.map((a) => (
+            <option value={a} key={a}>
+              {a}
+            </option>
+          ))}
+        </select>
+        <button onClick={() => refreshImage()}>Reload</button>
+      </div>
+      <div className="imagesContainer">
+        {images &&
+          images.map((image) => (
+            <div
+              key={image.id}
+              className="imageBlock"
+              style={{ width: `calc(100%/${images.length})` }}
+            >
+              <img src={image.url} alt="" />
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
